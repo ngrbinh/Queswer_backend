7a25e14d4195d489681b0bcbfabf3bd3285d2315
Mrbssd <43273921+Mrbssd@users.noreply.github.com>
2020-11-24 20:16:03 +0700

    cors8

--- src/main/java/com/bdt/queswer/security/jwt/JwtAuthorizationFilter.java
+++ src/main/java/com/bdt/queswer/security/jwt/JwtAuthorizationFilter.java
@@ -38,9 +38,7 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods","*");
-        response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, " +
-                "Origin,Accept, X-Requested-With, " +
-                "Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+        response.setHeader("Access-Control-Allow-Headers", "*");
         if (token == null || !token.startsWith(TOKEN_PREFIX)) {
             chain.doFilter(request,response);
             return;
