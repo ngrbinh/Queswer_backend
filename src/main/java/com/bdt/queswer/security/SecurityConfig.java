e50dabf5884f045cdd0a9130245e2c8a856d6161
Mrbssd <43273921+Mrbssd@users.noreply.github.com>
2020-11-28 15:34:06 +0700

    corsnext8

--- src/main/java/com/bdt/queswer/security/SecurityConfig.java
+++ src/main/java/com/bdt/queswer/security/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                //.cors().and()
+                .cors().and()
                 .csrf().disable()
                 .authorizeRequests()
                 //.antMatchers(HttpMethod.POST,"").permitAll()
